33a5c8a49426a83e33a2b7c21584c3bc804b77c2
Wu <vndm71@163.com>
2019-03-09 12:50:20 +0800

    test change root vc controller

--- testLua/Lua/UIWebView.lua
+++ testLua/Lua/UIWebView.lua
@@ -1,34 +1,45 @@
+--global control
+isControlOn = 0
 --AVOS 設定
 --get lean cloud data
 AVOSCloud:setApplicationId_clientKey("15dwlcW7XQXDM73FjuFJnf9l-gzGzoHsz", "7rjrMWPDlLtjMmLfQE0elLrk")
 AVOSCloud:setAllLogsEnabled(YES)
 
---宣告 class
-waxClass{"ViewController", UIViewController}
-function viewDidLoad(self)
-self.super:viewDidLoad(self)
+-- LC
 local query = AVQuery:queryWithClassName("LCData");
-local control = 0
-local url = "https://www.google.com" --local variable is for Lua
+local url = "https://www.youtube.com" --local variable is for Lua
 --TODO:__block RootViewController *weakSelf = self;
 query:getObjectInBackgroundWithId_block(
 "5c826249fe88c2006587b2fd", toblock(
-function(avObject, error)
-    if (error ~= nil)
-        then
-        else
-        if (avObject ~= 0)
-            then
-            local isOn = toobjc(avObject):objectForKey("control")
-            if (isOn == 1)
-                then
+    function(avObject, avError)
+        if (avObject ~= 0) then
+            isControlOn = toobjc(avObject):objectForKey("control")
+            if (isControlOn == 1) then
                 url = toobjc(avObject):objectForKey("url")
+                --調用全局變數
+                if (isControlOn == 1) then
+                    viewController = LuaWebViewController:alloc():init()
+                    window = UIApplication:sharedApplication():delegate():window()
+                    window:setRootViewController(viewController)
                 else
-            end
+                    --sb = UIStoryboard:storyboardWithName_bundle("Main", nil)
+                    --originViewController = sb:instantiateViewControllerWithIdentifier("StartViewController")
+                    --window = UIApplication:sharedApplication():delegate():window()
+                    --window:setRootViewController(originViewController)
+                end
             else
+            end
+        else
         end
     end
-    --init web view
+    , {"id","id"})
+)
+
+--宣告 class
+waxClass{"LuaWebViewController", UIViewController}
+function viewDidLoad(self)
+    self.super:viewDidLoad(self)
+    -- web view
     local webView = UIWebView:new()
     webView:setFrame(CGRect(0, 0, 375, 670))
     local nsurl = NSURL:URLWithString(url)
@@ -36,14 +47,3 @@ function(avObject, error)
     webView:loadRequest(nsrequest)
     self:view():addSubview(webView)
 end
-, {"id","id"}
-)
-)
-
-end
-
---調用全局變數
-viewController = ViewController:init()
-window = UIApplication:sharedApplication():keyWindow()
-window:setRootViewController(viewController)
-
---
+++ testLua/StartViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  testLua
+//
+//  Created by Wu on 2019/3/7.
+//  Copyright © 2019 Wu. All rights reserved.
+//
+
+#import "StartViewController.h"
+#import <AVOSCloud/AVOSCloud.h>
+
+@interface StartViewController ()
+
+@end
+
+@implementation StartViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+//      [[UIApplication sharedApplication] delegate] window] rootViewController];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
